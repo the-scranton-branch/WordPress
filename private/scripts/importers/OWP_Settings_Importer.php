7970fb8b656756fa60636cdc0eac100aa0bdb6da
Kyle Taylor <kyletaylored@gmail.com>
2021-09-23 09:34:20 -0500

    Fix commit

--- private/scripts/importers/OWP_Settings_Importer.php
+++ private/scripts/importers/OWP_Settings_Importer.php
@@ -9,6 +9,7 @@ class OWP_Settings_Importer {
 	 * Process import file - this parses the settings data and returns it.
 	 *
 	 * @param string $file path to json file.
+	 * @return false|mixed|string
 	 */
 	public function process_import_file( $file ) {
 
@@ -20,11 +21,11 @@ class OWP_Settings_Importer {
 			return $data;
 		}
 
-		// Get file contents and decode
+		// Get file contents and decode.
 		$raw  = file_get_contents( $file );
 		$data = @unserialize( $raw );
 
-		// Delete import file
+		// Delete import file.
 		unlink( $file );
 
 		// If wp_css is set then import it.
@@ -32,33 +33,34 @@ class OWP_Settings_Importer {
 			wp_update_custom_css_post( $data['wp_css'] );
 		}
 
-		// Import the data
-    	return $this->import_data( $data['mods'] );
+		// Import the data.
+		return $this->import_data( $data['mods'] );
 
 	}
 
 	/**
 	 * Sanitization callback
 	 *
+	 * @param $file
+	 *
+	 * @return mixed
 	 * @since 1.0.5
 	 */
 	private function import_data( $file ) {
 
-		// Import the file
+		// Import the file.
 		if ( ! empty( $file ) ) {
 
 			if ( '0' == json_last_error() ) {
 
-				// Loop through mods and add them
+				// Loop through mods and add them.
 				foreach ( $file as $mod => $value ) {
 					set_theme_mod( $mod, $value );
 				}
-
 			}
-
 		}
 
-		// Return file
+		// Return file.
 		return $file;
 
 	}
