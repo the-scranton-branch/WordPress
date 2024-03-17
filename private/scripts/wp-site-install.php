da0c2f8b67b3872f1ed0b326f42d692657dd857d
Kyle Taylor <kyle.taylor@pantheon.io>
2024-03-17 11:47:13 -0500

    Update scripts

--- private/scripts/wp-site-install.php
+++ private/scripts/wp-site-install.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * Increase php memory limit and max execution time.
+ *
+ */
+if (isset ($_ENV['PANTHEON_ENVIRONMENT'])) {
+    ini_set('memory_limit', '1024M');
+    ini_set('max_execution_time', 1000);
+}
+
 $req = pantheon_curl('https://api.live.getpantheon.com/sites/self/attributes', NULL, 8443);
 $meta = json_decode($req['body'], true);
 
--- private/scripts/wp-site-setup.php
+++ private/scripts/wp-site-setup.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * Increase php memory limit and max execution time.
+ *
+ */
+if (isset ($_ENV['PANTHEON_ENVIRONMENT'])) {
+    ini_set('memory_limit', '1024M');
+    ini_set('max_execution_time', 1000);
+}
+
 // Enable Ocean theme and plugins
 echo "Enabling WP plugins and themes...\n";
 passthru("wp theme activate blocksy");
