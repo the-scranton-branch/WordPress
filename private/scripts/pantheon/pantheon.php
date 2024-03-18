c490299ebed6fc779d87e3eb9289cc93f7de53df
Kyle Taylor <kyle.taylor@pantheon.io>
2024-03-18 00:55:13 -0500

    Simplify NR script

--- private/scripts/pantheon/pantheon.php
+++ private/scripts/pantheon/pantheon.php
@@ -12,19 +12,12 @@ if (isset($_POST['environment'])) {
     $req = pantheon_curl('https://api.live.getpantheon.com/sites/self/settings', '{"allow_cacheserver":true}', 8443, 'PUT');
   }
 
-  // Get bindings
-  $req = pantheon_curl('https://api.live.getpantheon.com/sites/self/environments/self/bindings', NULL, 8443);
-  $bindings = json_decode($req['body'], true);
-  $nr = false;
-  foreach ($bindings as $binding) {
-    if ($bindings['type'] == 'newrelic') {
-      $nr = true;
-      break;
-    }
-  }
+  // Get New Relic binding if available.
+  $req = pantheon_curl('https://api.live.getpantheon.com/sites/self/environments/self/bindings?type=newrelic', NULL, 8443);
+  $nr_bindings = json_decode($req['body'], true);
 
   // If New Relic not detected, enable New Relic.
-  if ($nr == false) {
+  if (empty($nr_bindings)) {
     $req = pantheon_curl('https://api.live.getpantheon.com/sites/self/workflows', '{"type":"enable_new_relic_for_site","params":{"converge":true}}', 8443, 'POST');
   }
 }
