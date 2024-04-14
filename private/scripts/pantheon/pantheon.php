26b8f2feb194151685d293646357abe1c384bf8b
Kyle Taylor <kyle.taylor@pantheon.io>
2024-04-13 22:45:00 -0500

    Don't auto enable NR

--- private/scripts/pantheon/pantheon.php
+++ private/scripts/pantheon/pantheon.php
@@ -13,11 +13,11 @@ if (isset($_POST['environment'])) {
   }
 
   // Get New Relic binding if available.
-  $req = pantheon_curl('https://api.live.getpantheon.com/sites/self/environments/self/bindings?type=newrelic', NULL, 8443);
-  $nr_bindings = json_decode($req['body'], true);
+  // $req = pantheon_curl('https://api.live.getpantheon.com/sites/self/environments/self/bindings?type=newrelic', NULL, 8443);
+  // $nr_bindings = json_decode($req['body'], true);
 
   // If New Relic not detected, enable New Relic.
-  if (empty($nr_bindings)) {
-    $req = pantheon_curl('https://api.live.getpantheon.com/sites/self/workflows', '{"type":"enable_new_relic_for_site","params":{"converge":true}}', 8443, 'POST');
-  }
+  // if (empty($nr_bindings)) {
+  //   $req = pantheon_curl('https://api.live.getpantheon.com/sites/self/workflows', '{"type":"enable_new_relic_for_site","params":{"converge":true}}', 8443, 'POST');
+  // }
 }
